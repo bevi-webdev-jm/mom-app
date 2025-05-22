9203f7c0fa557bf28f41d7778e77e150e218bbd4
John Michael Renon <jm.renon@DCMakati.local>
2025-05-22 10:20:34 +0800

    initial commit

---
+++ lang/vendor/adminlte/en/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'settings'          => 'Settings',
+    'companies'         => 'Companies',
+    'users'             => 'Users',
+    'roles'             => 'Roles',
+    'system_settings'   => 'System Settings',
+    'system_logs'       => 'System Logs',
+    'error_logs'        => 'Error Logs',
+];
---
+++ lang/vendor/adminlte/ja/utilities.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'search'            => '検索',
+    'name'              => '名前',
+    'email'             => 'メールアドレス',
+    'back'              => '戻る',
+    'view'              => '表示',
+    'edit'              => '編集',
+    'delete'            => '削除',
+    'required'          => '必須',
+    'status'            => '状態',
+    'offline'           => 'オフライン',
+    'online'            => 'オンライン',
+    'created_at'        => '作成日時',
+    'updated_at'        => '更新日時',
+    'system_settings'   => 'システム設定',
+    'data_per_page'     => '1ページあたりのデータ数',
+    'email_sending'     => 'メール送信',
+    'save_settings'     => '設定を保存',
+];
