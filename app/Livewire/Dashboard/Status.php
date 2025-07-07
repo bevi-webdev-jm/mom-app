af0c24a5ff28093c940819d92da3b610569005da
John Michael Renon <jm.renon@DCMakati.local>
2025-07-07 08:17:59 +0800

    dashboard status update

--- app/Livewire/Dashboard/Status.php
+++ app/Livewire/Dashboard/Status.php
@@ -11,17 +11,32 @@ class Status extends Component
     public function render()
     {
         $data = MomDetail::select(
-            'status',
-            DB::raw('COUNT(status) as total')
+            DB::raw("
+                CASE
+                    WHEN status = 'Open' AND DATE(NOW()) > target_date THEN 'Overdue'
+                    WHEN status = 'Completed' AND completed_date > target_date THEN 'Extended'
+                    WHEN status = 'Completed' AND completed_date <= target_date THEN 'On time'
+                    WHEN status = 'Open' AND DATE(NOW()) <= target_date THEN 'Open'
+                END as derived_status
+            "),
+            DB::raw('COUNT(*) as total')
         )
-        ->groupBy('status')
+        ->when(!auth()->user()->hasRole('superadmin') && !auth()->user()->hasRole('admin'), function($query) {
+            $query->whereHas('mom', function($qry) {
+                $qry->whereHas('participants', function($qry1) {
+                    $qry1->where('id', auth()->user()->id);
+                })
+                ->orWhere('user_id', auth()->user()->id);
+            });
+        })
+        ->groupBy('derived_status')
         ->get();
 
         $chartData = [];
         $totalTopic = 0;
         foreach($data as $val) {
             $chartData[] = [
-                'name' => $val->status,
+                'name' => $val->derived_status,
                 'y' => $val->total
             ];
             $totalTopic += $val->total;
